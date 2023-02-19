839a8c6836370fa8cd33b834929857beed63ace9
lizailyina <liza-ilina-13052005@mail.ru>
2023-02-18 22:18:55 -0600

    implement responsive about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './about.css'
 
+import { FaAward } from 'react-icons/fa'
+import { FaReact } from 'react-icons/fa'
+import { TbCertificate } from 'react-icons/tb'
+
+
+import ME from '../../assets/me-about.jpg'
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>
+        Get To Know
+      </h5>
+      <h2>
+        About Me
+      </h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <TbCertificate className='about__icon'/>
+              <h5>
+                Certified Professional
+              </h5>
+              <small>
+                Verified Meta Certificate
+              </small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>
+                Experience
+              </h5>
+              <small>
+                1+ Year
+              </small>
+            </article>
+            <article className='about__card'>
+              <FaReact className='about__icon'/>
+              <h5>
+                Projects
+              </h5>
+              <small>
+                10+ Complete
+              </small>
+            </article>
+
+          </div>
+          <p> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corporis eligendi qui officiis quo quasi distinctio veritatis blanditiis molestiae dolore sequi mollitia, dolor accusamus? Numquam expedita ducimus quisquam voluptas quae.
+          </p>
+
+          <a href="#contact" class="btn btn-primary">Let's Talk</a>
+      </div>
+      </div>
+    </section>
   )
 }
 
