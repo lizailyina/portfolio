a04c5041b2da9dccff13174ecba3127fa46917a2
lizailyina <liza-ilina-13052005@mail.ru>
2023-02-19 21:37:15 -0600

    implement responsive contact & footer components, add & integrate emailjs library

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './contact.css'
+import { AiOutlineMail } from 'react-icons/ai'
+import { RiMessengerLine } from 'react-icons/ri'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+  const email = React.useRef();
+  const name = React.useRef();
+  const message = React.useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const obj = {
+      name: name.current.value,
+      email: email.current.value,
+      message: message.current.value,
+    }
+    emailjs.send('service_pwimrfr', 'template_67xqw9n', obj, 'A2-mO8HOJ4vH7eYBV')
+  
+    name.current.value = "";
+    email.current.value = "";
+    message.current.value = "";
+
+  }
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>
+        Get In Touch
+      </h5>
+      <h2>
+        Contact Me
+      </h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+        <article className="contact__option">
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>lizailyina13052005@gmail.com</h5>
+            <a href="mailto:lizailyina13052005@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>lizailyina13052005@gmail.com</h5>
+            <a href="https://m.me/profileid" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>WhatApp</h4>
+            <h5>lizailyina13052005@gmail.com</h5>
+            <a href="https://api.whatsapp.com/send?phone=number" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/*END OF CONTACT OPTIONS*/}
+        <form onSubmit={sendEmail}>
+          <input ref={name} type="text" name='name' placeholder='Your Full Name' required />
+          <input ref={email} type='email' name='email' placeholder='Your Email' required />
+          <textarea ref={message} name='message' rows='7' placeholder='Your Message' required />
+          <button type="submit" className='btn btn-primary'> Send Message </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { SiGlassdoor } from 'react-icons/si'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="" className='footer__logo'>Liza Ilyina</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a target='_blank' href="https://github.com"><BsGithub /></a>
+        <a target='_blank' href="https://linkedin.com"><BsLinkedin /></a>
+        <a target='_blank' href="https://glassdoor.com"><SiGlassdoor /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          &copy; Liza Ilyina. All rights reserved.
+        </small>
+      </div>
+    </footer>
   )
 }
 
